1d610b809ffeeacbdaaa28da40f0846447b8f083
hoho00 <kihoyoung@Kis-MacBook-Air.local>
2021-10-13 15:57:26 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/danso_pilot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.danso_pilot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/midi_dump.dart
@@ -0,0 +1,20 @@
+#!/usr/bin/env dart
+
+library midi_dump;
+
+import 'dart:io';
+
+import 'package:tekartik_midi/midi_parser.dart';
+
+// Dump midi files info and events
+void main(List<String> args) {
+  for (var arg in args) {
+    final file = File(arg);
+    if (file.existsSync()) {
+      List<int> data = file.readAsBytesSync();
+      final parser = FileParser(MidiParser(data));
+      parser.parseFile();
+      parser.file.dump();
+    }
+  }
+}
